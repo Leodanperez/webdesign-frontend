fc3db06d92c341afd608b0592990c941e33a596f
Leodanperez <leodanperez.1998@gmail.com>
2020-09-21 18:31:11 -0500

    upload file

---
+++ js/query.js
@@ -0,0 +1,5 @@
+const btnSwitch = document.querySelector("#switch");
+btnSwitch.addEventListener('click', () => {
+  document.body.classList.toggle('dark');
+  btnSwitch.classList.toggle('active');
+})
